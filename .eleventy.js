41642b52da22921df00c970674f39b0da5aefac2
ambikajo <ambikajoshi@gmail.com>
2021-06-21 01:04:44 +0530

    pushing to git

--- .eleventy.js
+++ .eleventy.js
@@ -1,35 +1,15 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
-const Image = require("@11ty/eleventy-img");
 const embeds = require("eleventy-plugin-embed-everything");
 
-async function imageShortcode(src, alt, sizes) {
-  let metadata = await Image(src, {
-    widths: [300, 600],
-    formats: ["avif", "jpeg"]
-  });
-
-  let imageAttributes = {
-    alt,
-    sizes,
-    loading: "lazy",
-    decoding: "async",
-  };
-
-  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
-  return Image.generateHTML(metadata, imageAttributes);
-}
 
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPlugin(embeds);
 
-  eleventyConfig.addPassthroughCopy('/src/images')
-  eleventyConfig.addPassthroughCopy('admin')
-  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
-  eleventyConfig.addLiquidShortcode("image", imageShortcode);
-  eleventyConfig.addJavaScriptFunction("image", imageShortcode);
+  eleventyConfig.addPassthroughCopy('./src/images')
+  
   const {
     DateTime
   } = require("luxon");
@@ -48,6 +28,9 @@ module.exports = function(eleventyConfig) {
   });
 
   return {
-    dir: { input: 'src', output: '_site' }
+    passthroughFileCopy: true,
+    dir: { input: 'src', output: '_site' },
+
   };
+  
 };
